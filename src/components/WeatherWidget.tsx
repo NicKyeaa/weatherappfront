b23afdf9075e34a38d4e679647502c18e146bdde
Tin <tin.jurcevic@gmail.com>
2023-06-18 01:24:35 +0200

    Added fetch and rest of the url variables

--- src/components/WeatherWidget.tsx
+++ src/components/WeatherWidget.tsx
@@ -6,20 +6,28 @@ const { Title } = Typography;
 const { Search } = Input;
 
 const WeatherWidget: React.FC = () => {
-  const [location, setLocation] = useState("");
+  const [location, setLocation] = useState("City");
 
   // Fetching the lat,long for the location
-  const fetchLocation = async () => {
-    const geocodingURL: URL = new URL(
-      "https://geocoding-api.open-meteo.com/v1/search?name=Berlin&count=10&language=en&format=json"
-    );
-    const resultLocation = await axios.get("");
+  const fetchLocation = async (locationText: string) => {
+    console.log(locationText);
+    const geocodingURLString: string =
+      "https://geocoding-api.open-meteo.com/v1/search?name=" +
+      locationText +
+      "&count=10&language=en&format=json";
+    const encodedGeocodingURL = encodeURIComponent(geocodingURLString);
+    const resultLocation = await axios.get(encodedGeocodingURL);
+    console.log(resultLocation);
   };
 
   return (
     <>
       <Space direction='vertical'>
-        <Search placeholder='Select city' enterButton></Search>
+        <Search
+          placeholder='Select city'
+          enterButton
+          onSearch={fetchLocation}
+        ></Search>
         <Card>
           <Title level={3}>{location}</Title>
         </Card>
