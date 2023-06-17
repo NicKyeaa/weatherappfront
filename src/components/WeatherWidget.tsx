f25b3208bd86ee969a4fd096ff6f005dc636b136
Tin <tin.jurcevic@gmail.com>
2023-06-18 01:53:56 +0200

    Success with fetching the weatherforecast

--- src/components/WeatherWidget.tsx
+++ src/components/WeatherWidget.tsx
@@ -7,27 +7,46 @@ const { Search } = Input;
 
 const WeatherWidget: React.FC = () => {
   const [location, setLocation] = useState("City");
+  const [weatherData, setWeatherData] = useState({});
 
   // Fetching the lat,long for the location
-  const fetchLocation = async (locationText: string) => {
+  const fetchLocation = async (locationString: string) => {
     // Declaring a URL variable
     const geocodingURL: string =
       "https://geocoding-api.open-meteo.com/v1/search?name=" +
-      encodeURI(locationText) +
+      encodeURI(locationString) +
       "&count=10&language=en&format=json";
 
     // Fetching the data
     const resultLocation = await axios.get(geocodingURL);
     console.log(resultLocation);
-    setLocation(locationText);
+    setLocation(locationString);
 
     // Returning the lat and longitute
-    const lattitude: number = resultLocation.data.results[0].latitude;
-    const longitude: number = resultLocation.data.results[0].longitude;
-    const returnArray = [lattitude, longitude];
-    console.log(resultLocation.data.results[0]);
+    const lattitude: string = resultLocation.data.results[0].latitude;
+    const longitude: string = resultLocation.data.results[0].longitude;
+    const returnArray = { lattitude, longitude };
+    console.log(returnArray);
 
     return returnArray;
+  };
+
+  // Fetching the weather forecast data after getting the lat and longitute
+  const fetchWeatherForecast = async (locationString: string) => {
+    // Getting the lat and long results
+    const { lattitude, longitude } = await fetchLocation(locationString);
+
+    // Declaring a URL variable with the results from geocoding func
+    const weatherForecastURL: string =
+      "https://api.open-meteo.com/v1/forecast?latitude=" +
+      encodeURI(lattitude) +
+      "&longitude=" +
+      encodeURI(longitude) +
+      "&hourly=temperature_2m&current_weather=true&forecast_days=1";
+    // Fetching the data
+    const resultWeatherForecast = await axios.get(weatherForecastURL);
+    console.log(resultWeatherForecast);
+    setWeatherData(resultWeatherForecast);
   };
 
   return (
@@ -36,7 +55,7 @@ const WeatherWidget: React.FC = () => {
         <Search
           placeholder='Select city'
           enterButton
-          onSearch={fetchLocation}
+          onSearch={fetchWeatherForecast}
         ></Search>
         <Card>
           <Title level={3}>{location}</Title>
